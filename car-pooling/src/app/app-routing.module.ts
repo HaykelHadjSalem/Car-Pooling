3e44b4aa2247b3815fbc3f85f2c2d3538697a10e
BasmaBenNasr <bennasrbasma97@gmail.com>
2020-11-17 22:19:48 +0100

    contact comp

--- car-pooling/src/app/app-routing.module.ts
+++ car-pooling/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactComponent } from './components/contact/contact.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 
@@ -7,7 +8,8 @@ import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
 {path:'login', component: LoginComponent},
-{path:'register',component: RegisterComponent}
+{path:'register',component: RegisterComponent},
+{path:'contact', component: ContactComponent}
 ];
 
 @NgModule({
