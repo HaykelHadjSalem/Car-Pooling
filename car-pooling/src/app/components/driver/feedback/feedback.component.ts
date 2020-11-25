c1b4620b733a984b512e895105963768536fb226
HaykelHadjSalem <haykel_hadj@yahoo.fr>
2020-11-25 21:42:38 +0100

    display driver rides with information related to status and date

--- car-pooling/src/app/components/driver/feedback/feedback.component.ts
+++ car-pooling/src/app/components/driver/feedback/feedback.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { RideService } from 'src/app/services/ride.service';
+import { TokenStorageService } from 'src/app/services/token-storage.service';
 
 @Component({
   selector: 'app-feedback',
@@ -9,14 +10,28 @@ import { RideService } from 'src/app/services/ride.service';
 })
 export class FeedbackComponent implements OnInit {
   passengers: any[];
-  constructor(private router: Router, private route: ActivatedRoute, private rideService: RideService) { }
+  message: string;
+  rideId: number;
+  constructor(private router: Router, private route: ActivatedRoute, private rideService: RideService, private tokenStorageService: TokenStorageService) { }
 
   ngOnInit(): void {
-    const rideId = +this.route.snapshot.paramMap.get('id');
-    this.rideService.getRidePassengers(rideId).subscribe(passengers => {
+    this.rideId = +this.route.snapshot.paramMap.get('id');
+    this.rideService.getRidePassengers(this.rideId).subscribe(passengers => {
       console.log(passengers);
       this.passengers = passengers;
     })
+  }
+
+  sendFeedback(passengerId) {
+    const driverId = this.tokenStorageService.getUser().id;
+    console.log(passengerId, this.message, driverId, this.rideId);
+    const feedback = {
+      rideId: this.rideId,
+      driverId: driverId,
+      passengerId: passengerId,
+      message: this.message,
+      sender: 'driver'
+    }
   }
 
 }
